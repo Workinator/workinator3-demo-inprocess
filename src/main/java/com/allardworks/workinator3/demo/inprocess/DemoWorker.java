166301239cb1f47d0fd25d8b0b12e5cda92dd4d1
Jay Allard <jay_allard@ultimatesoftware.com>
2018-04-23 23:28:53 -0400

    todo

--- src/main/java/com/allardworks/workinator3/demo/inprocess/DemoWorker.java
+++ src/main/java/com/allardworks/workinator3/demo/inprocess/DemoWorker.java
@@ -5,12 +5,19 @@ import com.allardworks.workinator3.consumer.WorkerContext;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
 
+import java.io.Console;
+
 @RequiredArgsConstructor
 public class DemoWorker implements AsyncWorker {
+    private boolean lastHadWork = false;
     @Override
     public void execute(WorkerContext context) {
         val hasWork = DemoHelper.getHack().getPartitionHasWork(context.getAssignment().getPartitionKey());
         context.hasWork(hasWork);
+        if (hasWork != lastHadWork) {
+            System.out.println("Has work change: Partition " + context.getAssignment().getPartitionKey() + " = " + hasWork);
+            lastHadWork = hasWork;
+        }
         while (DemoHelper.getHack().getWorkerIsFrozen(context.getAssignment().getWorkerId().getConsumer().getConsumerId().getName(), context.getAssignment().getWorkerId().getWorkerNumber())) {
             try {
                 Thread.sleep(100);
